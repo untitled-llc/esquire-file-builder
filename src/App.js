1f1b0c7222e81c15277bcab498c19c734c908aa5
nate-untitled <nate@untitledfirm.com>
2020-06-11 17:21:13 -0400

    confiugure cognito and add ui

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import './App.css';
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+import { withAuthenticator } from '@aws-amplify/ui-react'
+
+Amplify.configure(awsExports);
 
 function App() {
   return (
     <div className="App">
       <form className="App-header">
       <label for="count">Choose count category</label>
+      <br/>
         <select id="count">
           <option value="count-1">Count 1</option>
           <option value="count-2">Count 2</option>
           <option value="count-3">Count 3</option>
           <option value="count-4">Count 4</option>
         </select>
+      <br />
         <button type="submit">Get Count</button>
       </form>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, true);
