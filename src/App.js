248869bfd986fc07ffafa55f1ade72405a08910e
nate-untitled <nate@untitledfirm.com>
2020-06-12 13:31:16 -0400

    S3 File upload test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import MyTheme from './MyTheme';
+import Upload from './Upload';
 import Amplify from "aws-amplify";
 import { withAuthenticator } from "aws-amplify-react";
 import awsExports from "./aws-exports";
@@ -11,16 +12,9 @@ function App() {
   return (
     <div className="App">
     <form className="App-header">
-    <label for="count">Choose count category</label>
+    <label for="count">Upload a png file</label>
     <br/>
-      <select id="count">
-        <option value="count-1">Count 1</option>
-        <option value="count-2">Count 2</option>
-        <option value="count-3">Count 3</option>
-        <option value="count-4">Count 4</option>
-      </select>
-    <br />
-      <button type="submit">Get Count</button>
+    <Upload />
     </form>
   </div>
   );
---
+++ src/Upload.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class S3ImageUpload extends React.Component {
+    onChange(e) {
+        const file = e.target.files[0];
+        Storage.put('example.png', file, {
+            contentType: 'image/png'
+        })
+        .then (result => console.log(result))
+        .catch(err => console.log(err));
+    }
+
+    render() {
+        return (
+            <input
+                type="file" accept='image/png'
+                onChange={(evt) => this.onChange(evt)}
+            />
+        )
+    }
+}
+
+export default S3ImageUpload;
